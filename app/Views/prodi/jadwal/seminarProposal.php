20f944c83271cde1a44c11a9413bb45783ed0b22
vanilla7121 <87371095+vanilla7121@users.noreply.github.com>
2021-07-16 23:07:15 +0700

    edit jadwal prodi
    isi tabel jadwal prodi

--- app/Views/prodi/jadwal/seminarProposal.php
+++ app/Views/prodi/jadwal/seminarProposal.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>DataTables</h1>
+                    <h1>Jadwal Seminar Proposal</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -26,13 +26,50 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Jadwal Seminar Proposal</h3>
-                        </div>
                         <!-- /.card-header -->
-                        <div class="card-body">
-                            ini apa isinya ya?
 
+                        <div class="card-body">
+                            <table id="example2" class="table table-bordered table-hover">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>
+                                            <center>No</center>
+                                        </th>
+                                        <th>
+                                            <center>NIM</center>
+                                        </th>
+                                        <th>
+                                            <center>Nama Mahasiswa</center>
+                                        </th>
+                                        <th>
+                                            <center>Judul</center>
+                                        </th>
+                                        <th>
+                                            <center>Deskripsi</center>
+                                        </th>
+                                        <th>
+                                            <center>Nama Dosen Pembimbing</center>
+                                        </th>
+                                        <th>
+                                            <center>Nama Dosen Penguji</center>
+                                        </th>
+                                        <th>
+                                            <center>Tanggal seminar Proposal</center>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>201811111</td>
+                                        <td>Alexander Pierce</td>
+                                        <td>Sistem pendukung keputusan pemilihan supplier pada CV. surya abadi dengan metode AHP</td>
+                                        <td>Sistem ini membantu para pengambil keputusan melihat keputusan mana yang paling baik untuk diambil</td>
+                                        <td>Thomas S.kom,M.kom</td>
+                                        <td>James S.kom,M.kom</td>
+                                        <td><input type="text" class="form-control" value="<?php echo date("j F Y"); ?>" name="tgl"></td>
+                                    </tr>
+                                </tbody>
                         </div>
                         <!-- /.card-body -->
                     </div>
