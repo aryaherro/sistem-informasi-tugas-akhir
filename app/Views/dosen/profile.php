2ff1fc1193da4d819586a54e5e79b35e2e44ceed
qoirudin <din08zhy@gmail.com>
2021-08-05 17:23:23 +0700

    ,

--- app/Views/dosen/profile.php
+++ app/Views/dosen/profile.php
@@ -3,86 +3,13 @@
 <?= $this->section('content'); ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <html>
-
-    <body>
-        <h1>Profil Mahasiswa</h1>
-        <p>
-            <td>Biodata Mahasiswa</td>
-        </p>
-
-        <form action="" method="">
-            <table>
-                <tr>
-                    <td>Nama</td>
-                    <td>:</td>
-                    <td><input type="text" nama="Nama"></td>
-                </tr>
-                <tr>
-                    <td>NIK</td>
-                    <td>:</td>
-                    <td><input type="number" nama="NIK"></td>
-                </tr>
-                <tr>
-                    <td>NIM</td>
-                    <td>:</td>
-                    <td><input type="number" nama="NIM"></td>
-                </tr>
-                <tr>
-                    <td>Jenis Kelamin</td>
-                    <td>:</td>
-                    <td>
-                        <input type="radio" nama="jk" value=> Laki-Laki
-                        <input type="radio" nama="jk" value=> Perempuan
-                    </td>
-                </tr>
-                <tr>
-                    <td>Tanggal Lahir</td>
-                    <td>:</td>
-                    <td><input type="date" nama="tanggal_lahir"></td>
-                </tr>
-                <tr>
-                    <td>Tempat Lahir</td>
-                    <td>:</td>
-                    <td><input type="text" nama="tempat_lahir"></td>
-                </tr>
-                <tr>
-                    <td>Alamat</td>
-                    <td>:</td>
-                    <td><textarea nama="alamat"></textarea></td>
-                </tr>
-                <tr>
-                    <td>No Telpn</td>
-                    <td>:</td>
-                    <td><input type="number" nama="no_telpn"></td>
-                </tr>
-
-                <tr>
-                    <td>Agama</td>
-                    <td>:</td>
-                    <td><input type="text" nama="agama"></td>
-                </tr>
-            </table>
-        </form>
-    </body>
-
-    </html>
     <section class="content-header">
         <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Blank Page</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Blank Page</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-        <!-- /.container-fluid -->
+
+            <h1>Profil Dosen</h1>
+
+
+            <!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
@@ -91,23 +18,79 @@
         <!-- Default box -->
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Title</h3>
+                <h3 class="card-title">
+                    <p>
+                        Biodata Dosen
+                    </p>
+                </h3>
 
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                </div>
+
             </div>
             <div class="card-body">
-                Start creating your amazing application!
+                <div class="row">
+                    <div class="col">
+                        <img src="/profile/<?= user()->__get('username'); ?>.png" height="400">
+                    </div>
+                    <div class="col">
+                        <form action="" method="">
+                            <table>
+                                <tr>
+                                    <td>Nama</td>
+                                    <td>:</td>
+                                    <td><input type="text" nama="Nama"></td>
+                                </tr>
+                                <tr>
+                                    <td>NIK</td>
+                                    <td>:</td>
+                                    <td><input type="number" nama="NIK"></td>
+                                </tr>
+                                <tr>
+                                    <td>NIM</td>
+                                    <td>:</td>
+                                    <td><input type="number" nama="NIM"></td>
+                                </tr>
+                                <tr>
+                                    <td>Jenis Kelamin</td>
+                                    <td>:</td>
+                                    <td>
+                                        <input type="radio" nama="jk" value=> Laki-Laki
+                                        <input type="radio" nama="jk" value=> Perempuan
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Tanggal Lahir</td>
+                                    <td>:</td>
+                                    <td><input type="date" nama="tanggal_lahir"></td>
+                                </tr>
+                                <tr>
+                                    <td>Tempat Lahir</td>
+                                    <td>:</td>
+                                    <td><input type="text" nama="tempat_lahir"></td>
+                                </tr>
+                                <tr>
+                                    <td>Alamat</td>
+                                    <td>:</td>
+                                    <td><textarea nama="alamat"></textarea></td>
+                                </tr>
+                                <tr>
+                                    <td>No Telpn</td>
+                                    <td>:</td>
+                                    <td><input type="number" nama="no_telpn"></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Agama</td>
+                                    <td>:</td>
+                                    <td><input type="text" nama="agama"></td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                </div>
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
-                Footer
+
             </div>
             <!-- /.card-footer-->
         </div>
--- app/Views/mahasiswa/profile.php
+++ app/Views/mahasiswa/profile.php
@@ -3,112 +3,101 @@
 <?= $this->section('content'); ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <html>
+    <section class="content-header">
+        <div class="container-fluid">
 
-    <body>
-        <h1>Profil Mahasiswa</h1>
-        <p>
-            <td>Biodata Mahasiswa</td>
-        </p>
+            <h1>Profil Mahasiswa</h1>
 
-        <form action="" method="">
-            <table>
-                <tr>
-                    <td>Nama</td>
-                    <td>:</td>
-                    <td><input type="text" nama="Nama"></td>
-                </tr>
-                <tr>
-                    <td>NIK</td>
-                    <td>:</td>
-                    <td><input type="number" nama="NIK"></td>
-                </tr>
-                <tr>
-                    <td>NIM</td>
-                    <td>:</td>
-                    <td><input type="number" nama="NIM"></td>
-                </tr>
-                <tr>
-                    <td>Jenis Kelamin</td>
-                    <td>:</td>
-                    <td>
-                        <input type="radio" nama="jk" value=> Laki-Laki
-                        <input type="radio" nama="jk" value=> Perempuan
-                    </td>
-                </tr>
-                <tr>
-                    <td>Tanggal Lahir</td>
-                    <td>:</td>
-                    <td><input type="date" nama="tanggal_lahir"></td>
-                </tr>
-                <tr>
-                    <td>Tempat Lahir</td>
-                    <td>:</td>
-                    <td><input type="text" nama="tempat_lahir"></td>
-                </tr>
-                <tr>
-                    <td>Alamat</td>
-                    <td>:</td>
-                    <td><textarea nama="alamat"></textarea></td>
-                </tr>
-                <tr>
-                    <td>No Telpn</td>
-                    <td>:</td>
-                    <td><input type="number" nama="no_telpn"></td>
-                </tr>
 
-                <tr>
-                    <td>Agama</td>
-                    <td>:</td>
-                    <td><input type="text" nama="agama"></td>
-                </tr>
-            </table>
-        </form>
-    </body>
+            <!-- /.container-fluid -->
+    </section>
 
-    </html>
+    <!-- Main content -->
+    <section class="content">
 
-    <ol class="breadcrumb float-sm-right">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item active">Blank Page</li>
-    </ol>
-</div>
-</div>
-</div>
-<!-- /.container-fluid -->
-</section>
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">
+                    <p>
+                        Biodata Mahasiswa
+                    </p>
+                </h3>
 
-<!-- Main content -->
-<section class="content">
 
-    <!-- Default box -->
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Title</h3>
-
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                    <i class="fas fa-times"></i>
-                </button>
             </div>
-        </div>
-        <div class="card-body">
-            Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-            Footer
-        </div>
-        <!-- /.card-footer-->
-    </div>
-    <!-- /.card -->
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <img src="/profile/<?= user()->__get('username'); ?>.png" height="400">
+                    </div>
+                    <div class="col">
+                        <form action="" method="">
+                            <table>
+                                <tr>
+                                    <td>Nama</td>
+                                    <td>:</td>
+                                    <td><input type="text" nama="Nama"></td>
+                                </tr>
+                                <tr>
+                                    <td>NIK</td>
+                                    <td>:</td>
+                                    <td><input type="number" nama="NIK"></td>
+                                </tr>
+                                <tr>
+                                    <td>NIM</td>
+                                    <td>:</td>
+                                    <td><input type="number" nama="NIM"></td>
+                                </tr>
+                                <tr>
+                                    <td>Jenis Kelamin</td>
+                                    <td>:</td>
+                                    <td>
+                                        <input type="radio" nama="jk" value=> Laki-Laki
+                                        <input type="radio" nama="jk" value=> Perempuan
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Tanggal Lahir</td>
+                                    <td>:</td>
+                                    <td><input type="date" nama="tanggal_lahir"></td>
+                                </tr>
+                                <tr>
+                                    <td>Tempat Lahir</td>
+                                    <td>:</td>
+                                    <td><input type="text" nama="tempat_lahir"></td>
+                                </tr>
+                                <tr>
+                                    <td>Alamat</td>
+                                    <td>:</td>
+                                    <td><textarea nama="alamat"></textarea></td>
+                                </tr>
+                                <tr>
+                                    <td>No Telpn</td>
+                                    <td>:</td>
+                                    <td><input type="number" nama="no_telpn"></td>
+                                </tr>
 
-</section>
-<!-- /.content -->
+                                <tr>
+                                    <td>Agama</td>
+                                    <td>:</td>
+                                    <td><input type="text" nama="agama"></td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+
+            </div>
+            <!-- /.card-footer-->
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 <?= $this->endSection(); ?>
